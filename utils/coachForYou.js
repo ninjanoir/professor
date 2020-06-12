6b0c3fcedb5c82e195c24f53cead1ff47724db20
ninjanoir <robinson.alain@free.fr>
2020-06-12 15:16:08 +0200

    config endpoint api

--- utils/coachForYou.js
+++ utils/coachForYou.js
@@ -2,14 +2,15 @@ import axios from 'axios'
 
 import getConfig from 'next/config'
 
-const {publicRuntimeConfig} = getConfig()
+const { publicRuntimeConfig } = getConfig()
 
-const URL = process.env.VERCEL_URL || publicRuntimeConfig.DOMAIN
+const URL =
+    process.env.NODE_ENV !== 'production'
+        ? `${publicRuntimeConfig.DOMAIN}/api`
+        : `${process.env.VERCEL_URL}/api`
 
-
-
-
+console.log(URL)
 
 export default axios.create({
-    baseURL: `${URL}/api`,
+    baseURL: URL,
 })
